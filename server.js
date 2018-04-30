bd94f38579238a5ab22c4d5a6109c11d9a847308
Mike <munkyu0813@gmail.com>
2018-04-30 12:44:00 -0400

    try using handlebars to create dynamic pages

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
