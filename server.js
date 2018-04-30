6a2ed245ff8f397a128fc211ed44dd092d865559
Mike <munkyu0813@gmail.com>
2018-04-30 12:38:16 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.send({
+    name: 'Mike',
+    likes: [
+      'Pizza',
+      'Music'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'This is the error message'
+  });
+});
+
+app.listen(3000);
